79d2037bd015e08632c6aa961baa62d454107d3e
777AMR <meyram.ramazanov@mail.ru>
2021-07-11 12:26:52 +0600

    Простой REST CRUD. Spring Boot + Webix

---
+++ src/main/java/letscode/logistic/repo/MarkRepo.java
@@ -0,0 +1,8 @@
+package letscode.logistic.repo;
+
+import letscode.logistic.domain.Mark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarkRepo extends JpaRepository<Mark, Long> {
+
+}
